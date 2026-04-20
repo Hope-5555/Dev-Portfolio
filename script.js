b9d1dec87241e66ac40c1922056ddcad055ba00e
Amos Kabba Kamara <amoskabbakamaradev@gmail.com>
2026-04-20 12:55:40 +0000

    added javaScrpt typing effect

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed("#typed", {
+  strings: [
+    "Web Developer",
+    "wordpress",
+    "Graphic Designer",
+    "UI/UX Design",
+    "Aspiring Backend Development",
+    "Photography/Photo Editing",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  startDelay: 500,
+  backDelay: 2000,
+  loop: true,
+  cursorChar: " ) :",
+});
